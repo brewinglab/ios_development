13f361515499b1bde0c25844b67445581ad3d335
WilliamHoang <william.sun.hoang@gmail.com>
2017-07-26 13:23:36 +0800

    homework to complete

--- tutorial/coredata/CoreDataStack_01/coredatastack_01/coredatastack_01/CDHelper.swift
+++ tutorial/coredata/CoreDataStack_01/coredatastack_01/coredatastack_01/CDHelper.swift
@@ -7,3 +7,72 @@
 //
 
 import Foundation
+import CoreData
+
+class CDHelper {
+    
+    
+    lazy var modelURL: NSURL = {
+        let bundle = Bundle.main
+        if let url = bundle.url(forResource: "Model", withExtension: "momd")  {
+            return url as NSURL
+            
+        }
+        
+        print ("Managed Object File not Found")
+        abort()
+    }()
+    
+    lazy var model: NSManagedObjectModel = {
+        return NSManagedObjectModel(contentsOf: self.modelURL as URL)!
+    }()
+    
+    lazy var storeDirectory: NSURL = {
+        let fm = FileManager.default
+        let urls = fm.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        return urls[urls.count - 1] as NSURL
+    }()
+    
+    lazy var localStoreURL:  NSURL = {
+        let url = self.storeDirectory.appendingPathComponent("CoreDataStack.sqlite")
+        return url! as NSURL
+    }()
+    
+    
+    lazy var coordinator:  NSPersistentStoreCoordinator = {
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.model)
+        
+        do {
+            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: self.localStoreURL as URL, options: nil)
+            
+        } catch {
+            print ("Could not add to the storage persistent")
+            abort()
+        }
+        
+        return coordinator
+        
+    }()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
